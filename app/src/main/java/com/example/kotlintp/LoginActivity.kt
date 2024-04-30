f23d78a3c1306fbf0a33f01b5a367f8c3179ebd1
ShortLegsFox <ian.bellot001@gmail.com>
2024-04-30 11:55:11 +0200

    Remove password input + change some strigns + username saved in SP with button is clicked (displayed in log)

--- app/src/main/java/com/example/kotlintp/LoginActivity.kt
+++ app/src/main/java/com/example/kotlintp/LoginActivity.kt
@@ -1,11 +1,42 @@
 package com.example.kotlintp
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var usernameInput : EditText
+    private lateinit var connectionButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        usernameInput = findViewById(R.id.username)
+        connectionButton = findViewById(R.id.login)
+        connectionButton.isEnabled = true
+
+        connectionButton.setOnClickListener {
+            saveUsernameToSharedPreferences()
+
+            val sharedPreferences: SharedPreferences = getSharedPreferences("AppData", MODE_PRIVATE)
+            val retrievedUsername = sharedPreferences.getString("username", "")
+            Log.d("LoginActivity", "Retrieved Username: $retrievedUsername")
+        }
+    }
+
+    private fun saveUsernameToSharedPreferences() {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("AppData", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        val username = usernameInput.text.toString()
+        editor.putString("username", username)
+        editor.apply()
     }
 }
