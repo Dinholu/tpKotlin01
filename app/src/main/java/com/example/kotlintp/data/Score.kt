ee7731eb34e9a905caba21c823c855462f3e036c
shortlegsfox <mister.dusbin777@gmail.com>
2024-05-28 11:33:47 +0200

    Foreign key on score dao

--- app/src/main/java/com/example/kotlintp/data/Score.kt
+++ app/src/main/java/com/example/kotlintp/data/Score.kt
@@ -1,8 +1,25 @@
 package com.example.kotlintp.data
 
-//@Entity(tablename = "scores")
+/*@Entity(tablename = "scores",
+    foreignKeys = [
+        ForeignKey(
+            entity = Utilisateur::class,
+            parentColumns = ["Id"],
+            childColumns = ["IdUtilisateur"],
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Categorie::class,
+            parentColumns = ["Id"],
+            childColumns = ["IdCategorie"],
+            onDelete = ForeignKey.CASCADE
+        )]
+)
+*/
 data class Score (
+    //@PrimaryKey
     val IdUtilisateur : Int,
+    //@PrimaryKey
     val IdCategorie : Int,
     val Score : Int,
 )
--- app/src/main/java/com/example/kotlintp/data/Utilisateur.kt
+++ app/src/main/java/com/example/kotlintp/data/Utilisateur.kt
@@ -2,6 +2,7 @@ package com.example.kotlintp.data
 
 //@Entity(tablename = "utilisateurs")
 data class Utilisateur (
+    //@PrimaryKey(autogenerate=true)
     val Id : Int,
     val Pseudo : String,
 )
